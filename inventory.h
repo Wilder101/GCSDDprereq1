6a53d0051c6e4691ffc94cce6a1a22873eee1155
GitWilder <43479032+Wilder101@users.noreply.github.com>
2023-05-15 19:49:58 -0700

    Header update

--- inventory.h
+++ inventory.h
@@ -24,7 +24,6 @@ using namespace std;
 // SoftDrinkInventory class declaration
 class SoftDrinkInventory 
 {
-    
   public:       
     SoftDrinkInventory();     // Constructor 
     ~SoftDrinkInventory();    // Destructor 
